496676a852dc0c314dd6d1bae9d4404d025c21c9
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2023-01-03 13:10:21 +0530

    js

--- todo.js
+++ todo.js
@@ -2,3 +2,5 @@ const tasks = [];
 const taskList = document.getElementById('list');
 const addTaskInput = document.getElementById('add');
 const taskCounter = document.getElementById('tasks-counter');
+
+console.log('working');
