c76181214b69bad90143cf0ceaadb46101f8a4c7
ANGADMADHEPURA <ANGADRAJ9534@GMAIL.COM>
2023-01-03 13:13:58 +0530

    add functionlaties

--- todo.js
+++ todo.js
@@ -4,3 +4,14 @@ const addTaskInput = document.getElementById('add');
 const taskCounter = document.getElementById('tasks-counter');
 
 console.log('working');
+
+function renderList (){}
+
+function markTaskAsComplete (taskId){}
+
+function deleteTask (taskId){}
+
+function addTask (task)()
+
+function showNotification (text){}
+
